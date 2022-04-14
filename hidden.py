3f819fbada1cab48255b4eeeb7a3a36321cfb282
Migueliot1 <migueliot@protonmail.com>
2022-04-15 00:24:39 +0300

    Add example of a file which hides databases names.

---
+++ hidden.py
@@ -0,0 +1,12 @@
+tickets_db = 'cinema.db'
+cards_db = 'banking.db'
+
+def get_tickets_db():
+    '''Returns name of the Tickets database.'''
+
+    return tickets_db
+
+def get_cards_db():
+    '''Returns name of the Cards database.'''
+
+    return cards_db
